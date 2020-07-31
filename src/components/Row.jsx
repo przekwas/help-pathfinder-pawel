834287431451b6d2cfbbb39ad9612c451e986a88
przekwas <luke@covalence.io>
2020-07-31 16:57:39 -0500

    changes

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tile from './Tile';
 
 const Row = ({ tileCount, ...rest }) => {
+	const [rowDim, setRowDim] = useState(false);
+
 	return (
 		<div className="row">
 			{tileCount.map((tile, i) => (
-				<Tile key={i} tile={tile} {...rest} />
+				<Tile
+					key={i}
+					tile={tile}
+					rowDim={rowDim}
+					setRowDim={setRowDim}
+					{...rest}
+				/>
 			))}
 		</div>
 	);
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 
-const Tile = ({ tile, dispatch }) => {
-    const [hitOrMiss, setHitOrMiss] = useState('');
-    const [clicked, setClicked] = useState(false);
+const Tile = ({ tile, rowDim, setRowDim, dispatch }) => {
+	const [hitOrMiss, setHitOrMiss] = useState('');
+	const [clicked, setClicked] = useState(false);
 
 	const tileClick = e => {
-        if(clicked) return;
+		if (clicked || rowDim) return;
 		if (Number(e.target.id) === 0) {
 			setHitOrMiss('right');
+			setRowDim(true);
 			dispatch({ type: 'right' });
 		} else {
 			setHitOrMiss('wrong');
 			dispatch({ type: 'wrong' });
-        }
-        setClicked(true);
+		}
+		setClicked(true);
 	};
 
-	return <div onClick={tileClick} className={`tile ${hitOrMiss}`} id={tile}></div>;
+	return (
+		<div onClick={tileClick} className={`tile-base tile ${hitOrMiss} ${rowDim ? 'dim' : ''}`} id={tile} />
+	);
 };
 
 export default Tile;
